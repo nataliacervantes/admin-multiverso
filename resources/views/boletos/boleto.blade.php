0e84bb298362a6321483977fa28637b9b3624562
BCP <hawaicincoceronomams@gmail.com>
2021-07-02 15:23:44 -0500

    se actualiza el boleto en pdf

--- resources/views/boletos/boleto.blade.php
+++ resources/views/boletos/boleto.blade.php
@@ -8,24 +8,37 @@
             .ticket{
                 border: 1px solid black;
                 width: 100%;
-                height: 300px;;
+                height: 300px;
+                font-family: Serna;
             }
             .info{
                 width: 65%;
-                background-image: url('{{public_path('/images/fondo.jpeg')}}');
-                border: 1px solid orange;
+                background-image: url('{{public_path('/img/fondo.jpeg')}}');
+                /* border: 1px solid orange; */
                 height: inherit;
                 display : inline-block;
                 position: absolute;
+                background-size: cover;
+                color: white;
+                opacity: .9;
             }
             .codigo{
                 height: inherit;
                 width: 35%;
-                border: 1px solid rgb(84, 84, 240);
+                /* border: 1px solid rgb(84, 84, 240); */
                 align : right;
                 display : inline-block;
                 position: absolute;
-                margin-left : 470px;;
+                margin-left : 456px;;
+            }
+            @font-face{
+                font-family: 'Serna';
+                src:
+                /* url({{ public_path('fonts/1942.ttf') }}) format("truetype"); */
+                url('{{public_path('fonts/1942.eot')}}' format(“embedded-opentype”),
+                url('{{public_path('fonts/1942.woff')}}' format(“woff”),
+                url('{{public_path('fonts/1942.ttf')}}' format(“truetype”),
+                url('{{public_path('fonts/1942.svg')}}' format(“svg”);
             }
         </style>
      </head>
@@ -33,26 +46,26 @@
      <body>
         <div class="ticket">
             <div class="info">
-                <div>
+                <div style="margin-bottom: 60px; margin-top: 35px; margin-left: 35px">
                     {{ $evento['Lugar']}}
                 </div>
-                <div style='text-align: right'>
-                    <p>José de la Serna</p>
-                    <label for="">{{ $evento['Evento']}}</label>
-                </div>
-                <div style="height : 30px; width: inheret"></div>
-                <div style='text-align: right'>
+                <div style="height : 70px; width: inheret"></div>
+                <div style='text-align: right; margin-right: 15px'>
                         @php
                             $date = Carbon::create($evento['Fecha'])->locale('es');
                         @endphp
                     <p>
-                        {{ $date->isoFormat('LL')}} a las {{$evento['Hora']}} <br><br>
-                        {{$evento['Domicilio']}} {{$evento['Ciudad']}} {{$evento['Estado']}}
+                        José de la Serna <br>
+                        {{ $evento['Evento'] }} <br>
+                        {{ $date->isoFormat('LL')}} a las {{$evento['Hora']}} <br>
+                        {{ $evento['Domicilio']}} {{$evento['Ciudad']}} {{$evento['Estado']}}
                     </p>
                 </div>
             </div>
             <div class="codigo">
-
+                <div class="visible-print text-center">
+                    {{-- <img src="data:image/png;base64, {{ base64_encode($message->embedData(QrCode::format('png')->generate($msg['Folio']), 'QrCode.png', 'images/png')) }} "> --}}
+                </div>
             </div>
         </div>
      </body>
--- resources/views/boletos/imprimir.blade.php
+++ resources/views/boletos/imprimir.blade.php
@@ -9,37 +9,34 @@
               </div>
             </div>
             <div class="row">
-              <div class="col-lg-12">
-                <section class="panel">
-                  <header class="panel-heading">
-                      <div class="col-lg-8">
-                          Generar Boletos
-                      </div>
-                    <div class="col-lg-4">
-                        <a href="{{ url('verEventos')}}" type="button"  class="btn btn-secondary">Crear evento</a>
-                    </div>
-                  </header>
-                  <div class="panel-body">
-                      @foreach ($eventos as $evento)
-                        {!! Form::open(['url'=>'generarBoleto']) !!}
-                            <div class="row" style="margin: 15px 0">
-                                <div class="col-md-4">
-                                    {!! Form::label('Evento', $evento->Evento) !!}
-                                </div>
-                                {!! Form::hidden('id', $evento->id) !!}
-                                <div class="col-md-2">
-                                    <button class="btn btn-info">Generar Boleto</button>
-                                </div>
+                <div class="col-lg-12">
+                    <section class="panel">
+                        <header class="panel-heading" >
+                            <div class="col-md-8" >
+                                Generar Boletos
                             </div>
-                        {!! Form::close() !!}
-                      @endforeach
-                  </div>
-                </section>
-              </div>
+                            <div class="col-md-4" >
+                                <a href="{{ url('verEventos')}}" type="button" class="btn btn-info">Crear evento</a>
+                            </div>
+                        </header>
+                        <div class="panel-body">
+                            @foreach ($eventos as $evento)
+                                {!! Form::open(['url'=>'generarBoleto']) !!}
+                                    <div class="row" style="margin: 15px 0">
+                                        <div class="col-md-4">
+                                            {!! Form::label('Evento', $evento->Evento) !!}
+                                        </div>
+                                        {!! Form::hidden('id', $evento->id) !!}
+                                        <div class="col-md-2">
+                                            <button class="btn btn-info">Generar Boleto</button>
+                                        </div>
+                                    </div>
+                                {!! Form::close() !!}
+                            @endforeach
+                        </div>
+                    </section>
+                </div>
             </div>
-
         </section>
-
     </section>
-
 @endsection
